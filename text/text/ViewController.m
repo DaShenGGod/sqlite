d54815de3f66a29d5a125e334675f603ec94612c
DashenGGod <xiaoshengzi222@163.com>
2016-03-25 14:25:54 +0800

    更新

--- text/text/ViewController.m
+++ text/text/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    int a = 100000;
+    NSLog(@"%d",a);
 }
 
 - (void)didReceiveMemoryWarning {
