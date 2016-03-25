059a3225fbf62628af2908fc2b42da32afaa00e5
DashenGGod <xiaoshengzi222@163.com>
2016-03-25 14:23:46 +0800

    嗷嗷嗷

---
+++ text/text/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 董文胜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ text/text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 董文胜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
